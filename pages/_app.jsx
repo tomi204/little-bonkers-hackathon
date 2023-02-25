cc8776459442985e848964529d3074ff494aa945
tomi204 <tomi204@outlook.com>
2023-02-25 10:29:51 -0300

    first commit

---
+++ pages/_app.jsx
@@ -0,0 +1,18 @@
+import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
+import { ThirdwebProvider } from "@thirdweb-dev/react/solana";
+import "../styles/globals.css";
+
+// Change the network to the one you want to use: "mainnet-beta", "testnet", "devnet", "localhost" or your own RPC endpoint
+const network = "mainnet-beta";
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <ThirdwebProvider network={network}>
+      <WalletModalProvider>
+        <Component {...pageProps} />
+      </WalletModalProvider>
+    </ThirdwebProvider>
+  );
+}
+
+export default MyApp;
